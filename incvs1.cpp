6229db7b298545efa44db838d08385d1e4d15335
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-03-26 20:11:54 +0700

    void insertionsort

--- incvs1.cpp
+++ incvs1.cpp
@@ -39,5 +39,14 @@ void insertionSort()
     int tempt;
     int j, i;
     for (i - 1; i <= n - 1; i++)
-    
+    {
+       temp = arr [i];
+       j = i - 1;
+       while (j >= 0 && arr[j] > temp) 
+       {
+        arr [j+ 1] = arr [j];
+        j--; 
+        }
+        arr [j + 1] = tempt;
+    }
 }
