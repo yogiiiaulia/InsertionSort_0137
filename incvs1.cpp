fe7757fee0c69fc07994fb5df157b68c8fe233cf
yogiiiaulia <yogiauliaardiano@gmail.com>
2025-03-26 19:59:34 +0700

    void input

--- incvs1.cpp
+++ incvs1.cpp
@@ -4,3 +4,30 @@ using namespace std;
 int arr[20]; //membuat array dengan panjang data 20
 int n; // Membuat variable inputan n
 
+void input()
+{ // procedure input
+    while (true)
+    {
+        cout << "masukan jumlah data pada array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\narray yang anda masukan maksimal 20 element.\n";
+        }
+    }
+    cout << endl;
+    cout << "==========" << endl;
+    cout << "masukan element array" << endl;
+    cout << "==========" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+    }
+}
